3f0ed5579921b65bb7d0672fe2ce3dd91cda5592
Belal Sryo <belalsryo@gmail.com>
2025-04-20 01:31:20 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "87544ed4-9d70-4b21-ba17-2bb576acccb1";
+export const MODAL_CLOSE_SEC = 2.5;
